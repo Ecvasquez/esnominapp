a0ac692f856143fb5a40fa80c86338a971792d51
Ecvasquez <ecvasquez@uninorte.edu.co>
2021-12-14 14:50:00 -0500

    first commit esnominapp

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import reactDom from 'react-dom';
+import { BrowserRouter,Switch } from "react-router-dom";
+// Importing the Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+import {App} from './App';
+
+reactDom.render(
+<BrowserRouter>
+        <App />
+</BrowserRouter>,
+document.getElementById('root'))
